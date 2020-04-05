200f0615ac991938a063813d837eb7ebfbcf6cf8
WidarD <243184@student.pwr.edu.pl>
2020-04-05 22:02:15 +0200

    commit to disable errors cause by empty functions

--- src/sample/database/worker_database_handler.java
+++ src/sample/database/worker_database_handler.java
@@ -7,24 +7,24 @@ import java.sql.SQLException;
 public class worker_database_handler
 {
     Connection databaseConnection;
-    public Connection get_database_connection() throws SQLException, ClassNotFoundException
-    {
-
-    }
+//    public Connection get_database_connection() throws SQLException, ClassNotFoundException
+//    {
+//
+//    }
 
     public void insert_worker() throws SQLException, ClassNotFoundException
     {
 
     }
-    public int get_all_workers() throws SQLException, ClassNotFoundException
-    {
+//    public int get_all_workers() throws SQLException, ClassNotFoundException
+//    {
+//
+//    }
 
-    }
-
-    public ResultSet get_workers_list() throws SQLException, ClassNotFoundException
-    {
-
-    }
+//    public ResultSet get_workers_list() throws SQLException, ClassNotFoundException
+//    {
+//
+//    }
 
 
     public void delete_worker() throws SQLException, ClassNotFoundException
