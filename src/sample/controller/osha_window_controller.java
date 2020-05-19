fd10df25bd92fa60252db3324b3ac516cc8b2c18
WidarD <243184@stduent.pwr.edu.pl>
2020-05-19 19:56:18 +0200

    Generate PDF of OSHA

--- src/sample/controller/osha_window_controller.java
+++ src/sample/controller/osha_window_controller.java
@@ -1,18 +1,28 @@
 package sample.controller;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.DatePicker;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import sample.pdf.osha_generator;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-public class osha_window_controller {
+public class osha_window_controller extends osha_generator {
     @FXML
     private ImageView back_icon;
+    @FXML
+    private JFXButton generate_button;
+    @FXML
+    private DatePicker data_pick_field;
+
     @FXML
     void initialize() {
         back_icon.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
@@ -32,5 +42,16 @@ public class osha_window_controller {
             primaryStage.setResizable(false);
             primaryStage.show();
         });
-    }
-}
+        generate_button.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
+            osha_generator osha_generator_usage = new osha_generator();
+            LocalDate date = data_pick_field.getValue();
+            String string_date = date.format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));
+
+            try {
+                osha_generator_usage.generate_pdf("Jan Kowalski", string_date);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }}
+
